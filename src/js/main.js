7929e1d173a4166a68714e34e40132d095f4be98
benjamin <benjamin.marissiaux@student.hepl.be>
2023-12-05 16:58:51 +0100

    js-tp-burger-menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ document.querySelector('nav').style.display = 'none';
 buttonSelector.style.display = 'block';
 
 buttonSelector.addEventListener('click', ()=>{
-    document.querySelector('nav').style.display = 'inline-block';
     buttonSelector.classList.toggle('is-opened');
+    document.querySelector('nav').style.display = 'inline-block';
 });
 
 
